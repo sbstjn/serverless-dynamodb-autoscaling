804af08b27ab6f79f4a6c825287ec17c77b23b28
sbstjn <mail@sbstjn.com>
2017-07-25 17:23:40 +0200

    Expand names.js function

--- src/aws/names.js
+++ src/aws/names.js
@@ -2,11 +2,70 @@ const util = require('util')
 
 const clean = (input) => input.replace(/[^a-z0-9+]+/gi, '')
 
-const policyScale = (table, read, index) => clean(util.format('Table%sScalingPolicy-%s%s', read ? 'Read' : 'Write', table, index || ''))
-const policyRole = (table, index) => clean(util.format('DynamoDBAutoscalePolicy-%s%s', table, index || ''))
-const dimension = (read, index) => util.format('dynamodb:%s:%sCapacityUnits', index ? 'index' : 'table', read ? 'Read' : 'Write')
-const target = (table, read, index) => clean(util.format('AutoScalingTarget%s-%s%s', read ? 'Read' : 'Write', table, index || ''))
-const metric = (read) => clean(util.format('DynamoDB%sCapacityUtilization', read ? 'Read' : 'Write'))
-const role = (table, index) => clean(util.format('DynamoDBAutoscaleRole-%s%s', table, index || ''))
+function policyScale (table, read, index) {
+  return clean(
+    util.format(
+      'Table%sScalingPolicy-%s%s', read ? 'Read' : 'Write',
+      table,
+      index || ''
+    )
+  )
+}
 
-module.exports = { dimension, metric, policyScale, policyRole, role, target, clean }
+function policyRole (table, index) {
+  return clean(
+    util.format(
+      'DynamoDBAutoscalePolicy-%s%s',
+      table,
+      index || ''
+    )
+  )
+}
+
+function dimension (read, index) {
+  return util.format(
+    'dynamodb:%s:%sCapacityUnits',
+    index ? 'index' : 'table',
+    read ? 'Read' : 'Write'
+  )
+}
+
+function target (table, read, index) {
+  return clean(
+    util.format(
+      'AutoScalingTarget%s-%s%s',
+      read ? 'Read' : 'Write',
+      table,
+      index || ''
+    )
+  )
+}
+
+function metric (read) {
+  return clean(
+    util.format(
+      'DynamoDB%sCapacityUtilization',
+      read ? 'Read' : 'Write'
+    )
+  )
+}
+
+function role (table, index) {
+  return clean(
+    util.format(
+      'DynamoDBAutoscaleRole-%s%s',
+      table,
+      index || ''
+    )
+  )
+}
+
+module.exports = {
+  clean,
+  dimension,
+  metric,
+  policyRole,
+  policyScale,
+  role,
+  target
+}
